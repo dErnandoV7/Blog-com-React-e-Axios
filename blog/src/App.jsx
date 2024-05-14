07d040e1c2c0409693c5ef6726193a2d06aa16a2
dErnandoV7 <d.ernandov7@gmail.com>
2024-05-14 08:07:52 -0300

    configurando rotas e aplicando estilos iniciais

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
+import "./App.css"
+
+function App() {
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- blog/src/main.jsx
+++ blog/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Children } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+import Home from "./routes/Home.jsx";
+import New from "./routes/New.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <New />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>
+);
